40f0c3a5b17237a807cf31bea8965a2002bd6b7b
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-11-15 12:51:50 +0100

    module skeleton

--- projects/module/src/app/app.component.ts
+++ projects/module/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {AlgorithmResult, DropFile, FD_LOG, PetriNetSerialisationService, Trace, XesLogParserService} from 'ilpn-components';
 
 @Component({
     selector: 'app-root',
@@ -6,4 +7,27 @@ import {Component} from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+    public fdLog = FD_LOG;
+
+    public log: Array<Trace> | undefined;
+    public resultFiles: Array<DropFile> = [];
+    public processing = false;
+
+    constructor(private _logParser: XesLogParserService,
+                private _netSerializer: PetriNetSerialisationService) {
+    }
+
+    public processLogUpload(files: Array<DropFile>) {
+        this.processing = true;
+        this.resultFiles = [];
+
+        const algorithmProtocol = new AlgorithmResult("ILP miner");
+
+        this.log = this._logParser.parse(files[0].content);
+        console.debug(this.log);
+
+
+    }
+
 }
